4e8de6c0d8b29cabb25a2d3a9f90fcddb497e146
Mathieu Ferment <mathieu.ferment@mylittleparis.com>
2014-08-13 14:30:40 +0200

    Add clients services

--- DependencyInjection/DlinZendeskExtension.php
+++ DependencyInjection/DlinZendeskExtension.php
@@ -24,6 +24,7 @@ class DlinZendeskExtension extends Extension {
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+        $loader->load('clients.xml');
 
 
         $container->setParameter(
